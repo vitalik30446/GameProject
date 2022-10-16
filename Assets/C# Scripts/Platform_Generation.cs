a8517b2b3b234cc788a1dc229472279aecfec510
Karasovskiy V <vitaliymir@ukr.net>
2022-10-16 19:13:57 +0300

    GameProject

---
+++ Assets/C# Scripts/Platform_Generation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Generation : MonoBehaviour
+{
+
+    public GameObject platformPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 SpawnerPosition = new Vector3();
+
+        for (int i = 0; i < 50; i++)
+        {
+            SpawnerPosition.x = Random.Range(-0.8f, 0.8f);
+            SpawnerPosition.y += Random.Range(0.6f, 0.6f);
+
+            Instantiate(platformPrefab, SpawnerPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
